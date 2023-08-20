679cb599c225783674f4f116d2a448e111b21ad5
colbs255 <colby@dev.com>
2023-08-20 00:49:14 -0400

    Save

--- src/sst.rs
+++ src/sst.rs
@@ -7,7 +7,43 @@ impl SST {
         self.extract_u32(0)
     }
 
-    fn extract_u32(&self, offset: usize) -> u32 {
+    fn key_offset(&self, key_index: u32) -> u32 {
+        self.extract_u32(4 + key_index * 4)
+    }
+
+    fn key_size(&self, key_index: u32) -> u32 {
+        let offset = self.key_offset(key_index);
+        self.extract_u32(offset)
+    }
+
+    fn key(&self, key_index: u32) -> &[u8] {
+        let offset = self.key_offset(key_index);
+        self.extract_string(offset)
+    }
+
+    fn get(&self, key: &Vec<u8>) -> Option<&[u8]> {
+        for i in 0..self.keys_size() {
+            let candidate_key = self.key(i);
+            if candidate_key.eq(key) {
+                return Some(self.value(i));
+            }
+        }
+        None
+    }
+
+    fn value_offset(&self, value_index: u32) -> u32 {
+        let key_offset = self.key_offset(value_index);
+        let key_size = self.key_size(value_index);
+        self.extract_u32(key_offset + key_size + 4)
+    }
+
+    fn value(&self, value_index: u32) -> &[u8] {
+        let offset = self.value_offset(value_index);
+        self.extract_string(offset)
+    }
+
+    fn extract_u32(&self, offset: u32) -> u32 {
+        let offset = offset as usize;
         u32::from_be_bytes([
             self.bytes[offset],
             self.bytes[offset + 1],
@@ -16,19 +52,11 @@ impl SST {
         ])
     }
 
-    fn key_offset(&self, key_index: u32) -> u32 {
-        self.extract_u32((4 + key_index * 4).try_into().unwrap())
-    }
-
-    fn key_size(&self, key_index: u32) -> u32 {
-        let offset = self.key_offset(key_index);
-        self.extract_u32(offset.try_into().unwrap())
-    }
-
-    fn key(&self, key_index: u32) -> &[u8] {
-        let offset: usize = (self.key_offset(key_index) + 4) as usize;
-        let size: usize = self.key_size(key_index) as usize;
-        &self.bytes[offset..offset + size]
+    fn extract_string(&self, offset: u32) -> &[u8] {
+        let string_len = self.extract_u32(offset) as usize;
+        let start = (offset + 4) as usize;
+        let end = start + string_len;
+        &self.bytes[start..end]
     }
 }
 
@@ -50,10 +78,15 @@ mod test {
             0,0,0,1,    /* size */
             3           /* value */
         );
+
         let sst = SST { bytes };
         assert_eq!(sst.keys_size(), 1);
         assert_eq!(sst.key_offset(0), 8);
         assert_eq!(sst.key_size(0), 1);
         assert_eq!(sst.key(0), vec!(2));
+        assert_eq!(sst.value_offset(0), 17);
+        assert_eq!(sst.value(0), vec!(3));
+        let result: [u8; 1] = [3];
+        assert_eq!(sst.get(&vec!(2)), Some(&result[0..1]));
     }
 }
